4447c6f979a46c0e40c82d7d5f409cf317b5a866
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2018-11-09 09:20:42 +0700

    get data success

--- app/src/main/java/poltekpos/ac/id/e_kuisoner/api/BaseApiService.java
+++ app/src/main/java/poltekpos/ac/id/e_kuisoner/api/BaseApiService.java
@@ -2,9 +2,19 @@ package poltekpos.ac.id.e_kuisoner.api;
 
 import poltekpos.ac.id.e_kuisoner.model.ResponsModel;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface BaseApiService {
+
+    @FormUrlEncoded
+    @POST("insert.php")
+    Call<ResponsModel> sendKuesioner(@Field("npm") String npm,
+                                   @Field("dosen") String dosen,
+                                   @Field("kuesioner") String kuesioner,
+                                   @Field("variable") String variable);
 
     @GET("read.php")
     Call<ResponsModel> getPertanyaan();
